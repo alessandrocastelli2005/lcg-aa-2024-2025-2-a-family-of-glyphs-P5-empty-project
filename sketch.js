3f703626d62f7380425a920d120ab2ba537312c3
alessandrocastelli2005 <alessandro8.castelli@mail.polimi.it>
2024-11-04 17:29:47 +0100

    aggiunta codice e file immagine di sfondo

--- sketch.js
+++ sketch.js
@@ -1,17 +1,48 @@
-function preload() {
-  // put preload code here
-}
+let bgImage; 
 
+function preload() {
+  bgImage = loadImage("pergamena.jpeg");
+}
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
+  noLoop();
+  background(255); 
 }
 
 function draw() {
-  // put drawing code here
+  image(bgImage, 0, 0, width, height);
+  let cols = 100; 
+  let rows = 8; 
+  let padding = 70; 
+  let startX = 30; 
+  let startY = 70; 
+
+  for (let i = 1; i < cols; i++) {
+    for (let j = 1; j < rows; j++) {
+      let x = startX + i * padding;
+      let y = startY + j * padding;
+      drawGlyph(x, y);
+    }
+  }
+}
+
+function drawGlyph(x, y) {
+  push();
+  translate(x, y);
+  let glyphSize = random(50, 50);
+
+  strokeWeight(random(2, 4));
+  stroke(0);
+  fill(0, 0, 0, 100);
+  
+  noFill();
+  beginShape();
+  for (let i = 0; i < random(6, 9); i++) {
+    let vx = random(-glyphSize / 2, glyphSize / 2);
+    let vy = random(-glyphSize / 2, glyphSize / 2);
+    curveVertex(vx, vy);
+  }
+  endShape();
+  
+  pop();
 }
